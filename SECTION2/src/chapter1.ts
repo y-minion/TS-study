06e98c24bb5d4bcfeeda513c6584e4b9a52cfed1
y-minion <dudals2963@tukrorea.ac.kr>
2025-08-14 17:58:01 +0900

    TS 섹션2_챕터 1 수업

---
+++ SECTION2/src/chapter1.ts
@@ -0,0 +1,23 @@
+//number 타입
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+//string 타입
+let str1: string = "helloooo";
+let str2: string = `hello ${str1}`;
+let str3: string = `hello ${num1}`;
+
+//boolean 타입
+let bool1: boolean = true;
+let bool2: boolean = false;
+
+//null 타입
+let null1: null = null;
+
+//리터럴 타입
+let numA: 10 = 10;
