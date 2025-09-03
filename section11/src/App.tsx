8ef1ad7f129fd74e5d95bc3967c688b75d44c648
y-minion <dudals2963@tukorea.ac.kr>
2025-09-03 16:47:50 +0900

    useReducer를 통해 상태 관리 방법 리팩토링

--- section11/src/App.tsx
+++ section11/src/App.tsx
@@ -1,26 +1,52 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useReducer, useRef } from "react";
 import "./App.css";
 import Editor from "./components/Editor";
 import { Todo } from "./types";
 import TodoItem from "./components/TodoItem";
-import { todo } from "node:test";
+
+//Action의 타입을 선언 이때 유니온 타입으로 설정해준다.
+type Action =
+  | {
+      type: "CREATE";
+      data: {
+        id: number;
+        content: string;
+      };
+    }
+  | {
+      type: "DELETE";
+      id: number;
+    };
+
+//reducer의 반환 값은 새로운 상태로 업데이트 된다.
+function reducer(state: Todo[], action: Action) {
+  switch (action.type) {
+    case "CREATE":
+      return [...state, action.data];
+    case "DELETE":
+      return state.filter((todo) => todo.id !== action.id);
+  }
+}
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, dispatch] = useReducer(reducer, []);
 
   const idRef = useRef(0);
   const onClickBtn = (text: string) => {
-    setTodos([
-      ...todos,
-      {
+    dispatch({
+      type: "CREATE",
+      data: {
         id: idRef.current++,
         content: text,
       },
-    ]);
+    });
   };
 
   const deleteTodo = (id: number) => {
-    setTodos(() => todos.filter((todo) => todo.id !== id));
+    dispatch({
+      type: "DELETE",
+      id: id,
+    });
   };
 
   useEffect(() => {
